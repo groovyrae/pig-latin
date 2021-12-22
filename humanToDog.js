0f330361246f31c0928142efda6033a8e6713027
Rae <93627683+groovyrae@users.noreply.github.com>
2021-12-22 16:29:08 -0500

    Add files via upload

---
+++ humanToDog.js
@@ -0,0 +1,15 @@
+//my age today
+var myAge = 14;
+//first 2 years of dog to human
+let earlyYears = 2;
+earlyYears = earlyYears * 10.5;
+//rest of years of dog to human
+let laterYears = myAge - 2;
+//dog years pass by 4
+laterYears *= 4;
+//self-explanatory
+myAgeInDogYears = earlyYears + laterYears;
+//my name 
+var myName = 'Raegen Ellis'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
