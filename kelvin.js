c8ea4bf2cb3a2e84842c0d3049ffd31ebdd060a6
Rae <93627683+groovyrae@users.noreply.github.com>
2022-06-14 13:47:39 -0400

    Delete kelvin.js

--- kelvin.js
+++
@@ -1,9 +0,0 @@
-//forecast in kelvin
-const kelvin = 0
-//celsius to kelvin
-var celsius = kelvin - 273
-//celsius to fahrenheit
-let fahrenheit = celsius * (9/5) + 32
-//converts previous fahrenheit number to decimal
-fahrenheit = Math.floor(fahrenheit)
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
